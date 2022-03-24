ebb1ba64a1d4dea318387d38fe52820a8797be6f
Daniel Weiler <danielweiler78@gmail.com>
2022-03-24 13:07:05 +0200

    Add functionality to and refactor 'findAvailability' function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,7 +17,9 @@ export function getNextDayOfTheWeek(
   excludeToday = false,
   refDate = new Date(userCurrentDateTime)
 ) {
-  const dayOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'].indexOf(dayName.slice(0,2))
+  const dayOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'].indexOf(
+    dayName.slice(0, 2)
+  )
   if (dayOfWeek < 0) return
   refDate.setHours(0, 0, 0, 0)
   refDate.setDate(
@@ -44,74 +46,123 @@ export async function getUserTimeZone() {
     auth: oAuth2Client,
     calendarId: 'primary',
   })
-  
+
   return cal.data.timeZone
 }
 
-// This function finds the next available time slot on the user's calendar for 
+export async function freeBusy(queryStartTime: string, queryEndTime: string) {
+  const availabilityQuery = await calendar.freebusy.query({
+    auth: oAuth2Client,
+    requestBody: {
+      timeMin: queryStartTime,
+      timeMax: queryEndTime,
+      timeZone: await getUserTimeZone(),
+      items: [
+        {
+          id: 'primary',
+        },
+      ],
+    },
+  })
+
+  const unavailableTimes = availabilityQuery.data.calendars?.primary.busy
+  assertDefined(unavailableTimes)
+
+  return unavailableTimes
+}
+
+export function checkTimeDuration(
+  timeSlotStart: string | null | undefined,
+  timeSlotEnd: string | null | undefined
+) {
+  assertDefined(timeSlotStart)
+  assertDefined(timeSlotEnd)
+  const availableTime =
+    (new Date(timeSlotEnd).getTime() - new Date(timeSlotStart).getTime()) /
+    60000
+  return availableTime
+}
+
+// This function finds the next available time slot on the user's calendar for
 // an event to be scheduled.
-//let queryDayCount = 0 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export async function findAvailability(queryStartTimeString: string | Date, eventDuration: number) {
-  const queryStartTime = new Date(queryStartTimeString)
-  queryStartTime.setDate(queryStartTime.getDate() + 0/* queryDayCount */) 
-  
-  const queryEndTime = new Date(queryStartTime)
-  queryEndTime.setHours(24,0,0,0)
-  
-  // Begin loop to iterate over the following days from the given start time 
-  let scheduling = true
-  while (scheduling) {
-    // Enables searching from the given time on the given day and from the 
+export async function findAvailability(
+  givenQueryStartTime: string | Date,
+  eventDuration: number
+) {
+  // Begin loop to iterate over the days from the given start time
+  let findingAvailability = true
+  let queryDayCount = 0
+  while (findingAvailability) {
+    const queryStartTimeDate = new Date(givenQueryStartTime)
+
+    // Set <queryStartTime> to current day being queried for availability
+    queryStartTimeDate.setDate(queryStartTimeDate.getDate() + queryDayCount)
+
+    // Enables searching from the given time on the given day and from the
     // beginning of the day on following days
-    /* if (queryDayCount > 0) {
-      queryStartTime.setHours(0,0,0,0)
-    } */
+    if (queryDayCount > 0) {
+      queryStartTimeDate.setHours(0, 0, 0, 0)
+    }
 
-    const availabilityQuery = await calendar.freebusy.query({
-      auth: oAuth2Client,
-      requestBody: {
-        timeMin: queryEndTime.toISOString(),
-        timeMax: queryEndTime.toISOString(),
-        timeZone: await getUserTimeZone(),
-        items: [
-          {
-            id: 'primary'
-          }
-        ]
-      },
-    })
-    console.log('DATA::', availabilityQuery.data)
-    console.log('BUSY::', availabilityQuery.data.calendars?.primary.busy)
+    const queryEndTimeDate = new Date(queryStartTimeDate)
+    queryEndTimeDate.setHours(24, 0, 0, 0)
 
-    const unavailableTimes = availabilityQuery.data.calendars?.primary.busy
-    assertDefined(unavailableTimes)
-    assertDefined(unavailableTimes[0].start)
-    
+    const queryStartTime = queryStartTimeDate.toISOString()
+    const queryEndTime = queryEndTimeDate.toISOString()
+    const unavailableTimes = await freeBusy(queryStartTime, queryEndTime)
+
     // Check if there are any busy times within the queried time slot
     if (unavailableTimes.length === 0) {
-      console.log('not busy')
-      // schedule event or just return start time - leave this til later until you know what you're doing when changing work hours 
-      
+      findingAvailability = false
+      return queryStartTime
     } else {
-      // Check if there is enough time for the event from the start of the 
-      // queried time slot to the start of the first busy time
-      const availableTime = (new Date(unavailableTimes[0].start).getTime() - queryStartTime.getTime()) / 60000
-      console.log(availableTime)
+      // Begin loop to iterate over the busy times in the <unavailableTimes>
+      // array to continue to check for available time within the queried time
+      for (let i = 0; i < unavailableTimes.length; i++) {
+        const event = unavailableTimes[i]
 
-      if (availableTime > eventDuration) {
-        console.log('enough time')
+        // Check if there is enough time for the event from the start of the
+        // queried time slot to the start of the first busy time
+        if (i === 0) {
+          const availableTime = checkTimeDuration(queryStartTime, event.start)
+          if (availableTime >= eventDuration) {
+            findingAvailability = false
+            return queryStartTime
+          }
+        }
 
-        // schedule event or just return start time - leave this til later until you know what you're doing when changing work hours 
-
-        return queryStartTime
-      } else {
-        console.log('not enough time')
-
-
+        // Check if there is another busy time in the <unavailableTimes> array
+        if (unavailableTimes[i + 1]) {
+          // If so, check if there is enough time for the event in between
+          // these two busy times
+          const nextEvent = unavailableTimes[i + 1]
+          const availableTime = checkTimeDuration(event.end, nextEvent.start)
+          if (availableTime >= eventDuration) {
+            findingAvailability = false
+            return event.end
+          } else {
+            continue
+          }
+        } else {
+          // If not, check if there is enough time for the event from the end
+          // of the last busy time to the end of the queried time slot
+          const availableTime = checkTimeDuration(event.end, queryEndTime)
+          if (availableTime >= eventDuration) {
+            findingAvailability = false
+            return event.end
+          }
+        }
       }
     }
-    scheduling = false
+    if (queryDayCount === 180) {
+      console.log(
+        'Message for user: "Reminder could not be scheduled. No available \
+        time could be found in the next 6 months"'
+      )
+      break
+    }
+
+    queryDayCount += 1
   }
+  return
 }

323eeb4539dfe8e641ba75d5e37cd3700bbb7a2a
Daniel Weiler <danielweiler78@gmail.com>
2022-03-29 13:07:41 +0300

    Fix misused promise bug in sign-in.route.ts

--- src/controllers/sign-in.controller.ts
+++ src/controllers/sign-in.controller.ts
@@ -2,8 +2,8 @@ import { Response } from 'express'
 import signInService from '../services/sign-in.service'
 import { SignInRequest } from '../types'
 
-async function signIn(req: SignInRequest, _res: Response) {
-  await signInService.signIn(req.body)
+function signIn(req: SignInRequest, _res: Response) {
+  signInService.signIn(req.body)
 }
 
 export default { signIn }
--- src/services/sign-in.service.ts
+++ src/services/sign-in.service.ts
@@ -8,37 +8,39 @@ require('express-async-errors')
 let signedInUser: string | undefined = ''
 export let userCurrentDateTime: Date
 
-async function signIn(data: SignInData) {
-  const { code } = data
-  const { tokens } = await oAuth2Client.getToken(code)
+function signIn(data: SignInData) {
+  void (async () => {
+    const { code } = data
+    const { tokens } = await oAuth2Client.getToken(code)
 
-  // According to the Google OAuth 2.0 documentation, the "sub" field of the
-  // ID token is the unique-identifier key for Google users.
-  assertDefined(tokens.id_token)
-  const jwtObject = jwtDecode<JwtPayload>(tokens.id_token)
-  signedInUser = jwtObject.sub
+    // According to the Google OAuth 2.0 documentation, the "sub" field of the
+    // ID token is the unique-identifier key for Google users.
+    assertDefined(tokens.id_token)
+    const jwtObject = jwtDecode<JwtPayload>(tokens.id_token)
+    signedInUser = jwtObject.sub
 
-  // The refresh token of a user needs to be saved for authorization of
-  // actions of a user. It is only given when a new one is needed.
-  console.log('Signed in!')
-  if (tokens.refresh_token !== undefined) {
-    await RefreshTokenModel.find({ user: signedInUser }).deleteOne()
-    console.log('Any old refresh token deleted')
+    // The refresh token of a user needs to be saved for authorization of
+    // actions of a user. It is only given when a new one is needed.
+    console.log('Signed in!')
+    if (tokens.refresh_token !== undefined) {
+      await RefreshTokenModel.find({ user: signedInUser }).deleteOne()
+      console.log('Any old refresh token deleted')
 
-    await new RefreshTokenModel({
-      refreshToken: tokens.refresh_token,
-      user: signedInUser,
-    }).save()
-    console.log('New refresh token saved')
-  }
+      await new RefreshTokenModel({
+        refreshToken: tokens.refresh_token,
+        user: signedInUser,
+      }).save()
+      console.log('New refresh token saved')
+    }
 
-  ({ userCurrentDateTime } = data)
+    ({ userCurrentDateTime } = data)
 
-  const query = await RefreshTokenModel.find({ user: signedInUser })
-  const refreshToken = query[0].refreshToken
-  oAuth2Client.setCredentials({
-    refresh_token: refreshToken,
-  })
+    const query = await RefreshTokenModel.find({ user: signedInUser })
+    const refreshToken = query[0].refreshToken
+    oAuth2Client.setCredentials({
+      refresh_token: refreshToken,
+    })
+  })()
 }
 
 export default { signIn }

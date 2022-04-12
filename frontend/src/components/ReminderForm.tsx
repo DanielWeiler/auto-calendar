b1e92d19aea4767f5a072cf914800191c68c1c0c
Daniel Weiler <danielweiler78@gmail.com>
2022-04-12 17:40:25 +0300

    Refactor durationOptions object to array

--- frontend/src/components/ReminderForm.tsx
+++ frontend/src/components/ReminderForm.tsx
@@ -17,41 +17,39 @@ const ReminderForm = () => {
     },
   })
 
-  const durationOptions = {
-    options: [
-      { value: '5', text: '5 minutes' },
-      { value: '10', text: '10 minutes' },
-      { value: '15', text: '15 minutes' },
-      { value: '20', text: '20 minutes' },
-      { value: '25', text: '25 minutes' },
-      { value: '30', text: '30 minutes' },
-      { value: '35', text: '35 minutes' },
-      { value: '40', text: '40 minutes' },
-      { value: '45', text: '45 minutes' },
-      { value: '50', text: '50 minutes' },
-      { value: '60', text: '1 hour' },
-      { value: '75', text: '1 hr 15 min' },
-      { value: '90', text: '1 hr 30 min' },
-      { value: '105', text: '1 hr 45 min' },
-      { value: '120', text: '2 hours' },
-      { value: '135', text: '2 hr 15 min' },
-      { value: '150', text: '2 hr 30 min' },
-      { value: '165', text: '2 hr 45 min' },
-      { value: '180', text: '3 hours' },
-      { value: '195', text: '3 hr 15 min' },
-      { value: '210', text: '3 hr 30 min' },
-      { value: '225', text: '3 hr 45 min' },
-      { value: '240', text: '4 hours' },
-      { value: '270', text: '4 hr 30 min' },
-      { value: '300', text: '5 hours' },
-      { value: '330', text: '5 hr 30 min' },
-      { value: '360', text: '6 hours' },
-      { value: '390', text: '6 hr 30 min' },
-      { value: '420', text: '7 hours' },
-      { value: '450', text: '7 hr 30 min' },
-      { value: '480', text: '8 hours' },
-    ],
-  }
+  const durationOptions = [
+    { value: '5', text: '5 minutes' },
+    { value: '10', text: '10 minutes' },
+    { value: '15', text: '15 minutes' },
+    { value: '20', text: '20 minutes' },
+    { value: '25', text: '25 minutes' },
+    { value: '30', text: '30 minutes' },
+    { value: '35', text: '35 minutes' },
+    { value: '40', text: '40 minutes' },
+    { value: '45', text: '45 minutes' },
+    { value: '50', text: '50 minutes' },
+    { value: '60', text: '1 hour' },
+    { value: '75', text: '1 hr 15 min' },
+    { value: '90', text: '1 hr 30 min' },
+    { value: '105', text: '1 hr 45 min' },
+    { value: '120', text: '2 hours' },
+    { value: '135', text: '2 hr 15 min' },
+    { value: '150', text: '2 hr 30 min' },
+    { value: '165', text: '2 hr 45 min' },
+    { value: '180', text: '3 hours' },
+    { value: '195', text: '3 hr 15 min' },
+    { value: '210', text: '3 hr 30 min' },
+    { value: '225', text: '3 hr 45 min' },
+    { value: '240', text: '4 hours' },
+    { value: '270', text: '4 hr 30 min' },
+    { value: '300', text: '5 hours' },
+    { value: '330', text: '5 hr 30 min' },
+    { value: '360', text: '6 hours' },
+    { value: '390', text: '6 hr 30 min' },
+    { value: '420', text: '7 hours' },
+    { value: '450', text: '7 hr 30 min' },
+    { value: '480', text: '8 hours' },
+  ]
 
   const onSubmit = async (data: ReminderFormValues) => {
     const { manualDate, manualTime, deadlineDate, deadlineTime } = data
@@ -123,7 +121,7 @@ const ReminderForm = () => {
           id="duration"
           {...register('duration', { required: true })}
         >
-          {durationOptions.options.map((option) => (
+          {durationOptions.map((option) => (
             <option key={option.value} value={option.value}>
               {option.text}
             </option>

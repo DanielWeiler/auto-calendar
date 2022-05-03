5c7b6fcad3004d0059c189fc082aafb06a468002
Daniel Weiler <danielweiler78@gmail.com>
2022-05-03 13:47:07 +0300

    Update and fix tests

--- frontend/src/tests/WorkWeekForm.test.tsx
+++ frontend/src/tests/WorkWeekForm.test.tsx
@@ -1,57 +1,60 @@
 import '@testing-library/jest-dom'
-import { render, RenderResult } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import WorkDayForm from '../components/WeekDayForm'
-import WorkingHoursForm from '../components/WorkWeekForm'
-import { assertDefined } from '../utils/helpers'
+import WorkWeekForm from '../components/WorkWeekForm'
 
-describe('WorkingHoursForm', () => {
-  let component: RenderResult<
-    typeof import('@testing-library/dom/types/queries'),
-    HTMLElement
-  > | null = null
-
-  let innerComponent: RenderResult<
-    typeof import('@testing-library/dom/types/queries'),
-    HTMLElement
-  > | null = null
-
+describe('WorkWeekForm', () => {
+  const mockCreateNotification = jest.fn()
   const mockRegister = jest.fn()
   const mockError = {
     type: '',
     message: '',
   }
 
-  beforeEach(() => {
-    component = render(<WorkingHoursForm />)
-    innerComponent = render(
+  test('should render its elements', () => {
+    const workWeekForm = render(
+      <WorkWeekForm createNotification={mockCreateNotification} />,
+      { wrapper: MemoryRouter }
+    )
+
+    // Check for the week day selectors
+    expect(workWeekForm.getByRole('checkbox', { name: 'M' }))
+    // Get the name for Tuesday and Thursday
+    expect(workWeekForm.getAllByRole('checkbox', { name: 'T' }))
+    expect(workWeekForm.getByRole('checkbox', { name: 'W' }))
+    expect(workWeekForm.getByRole('checkbox', { name: 'F' }))
+    // Get the name for Saturday and Sunday
+    expect(workWeekForm.getAllByRole('checkbox', { name: 'S' }))
+    expect(workWeekForm.getByText('Save'))
+
+    // Check for the week day labels
+    expect(workWeekForm.getByText('Monday'))
+    expect(workWeekForm.getByText('Tuesday'))
+    expect(workWeekForm.getByText('Wednesday'))
+    expect(workWeekForm.getByText('Thursday'))
+    expect(workWeekForm.getByText('Friday'))
+    expect(workWeekForm.getByText('Saturday'))
+    expect(workWeekForm.getByText('Sunday'))
+  })
+
+  test('should register a start time and an end time for a work day', () => {
+    const workDayForm = render(
       <WorkDayForm
-        day='(weekDayName)'
-        display=''
+        day="(weekDayName)"
+        display=""
         register={mockRegister}
         error={mockError}
       />
     )
-  })
 
-  test('should render its elements', () => {
-    assertDefined(component)
-    expect(component.container.querySelector('form')).toBeInTheDocument()
-    expect(component.container.querySelector('#save')).toBeInTheDocument()
-
-    assertDefined(innerComponent)
+    // Check for the work day's elements and that the times are registered
+    expect(workDayForm.container.querySelector('#dayName')).toBeInTheDocument()
     expect(
-      innerComponent.container.querySelector('#dayName')
+      workDayForm.container.querySelector('#startTime')
     ).toBeInTheDocument()
-    expect(
-      innerComponent.container.querySelector('#startTime')
-    ).toBeInTheDocument()
-    expect(
-      innerComponent.container.querySelector('#endTime')
-    ).toBeInTheDocument()
-  })
-
-  test('should register a start time and an end time for a work day', () => {
+    expect(workDayForm.container.querySelector('#endTime')).toBeInTheDocument()
     expect(mockRegister).toHaveReturnedTimes(2)
   })
 })
